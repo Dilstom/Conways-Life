ed9329a955bd50dce368234914da05ab3ebe5791
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-11-01 12:36:53 -0700

    Updates

---
+++ precache-manifest.45db2e9826e8f65572a9a726ea129936.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "f32d1a3631ea78450cf3",
+    "url": "/Conways-Life/static/js/main.f32d1a36.chunk.js"
+  },
+  {
+    "revision": "86e2d1e02c5c16b087d9",
+    "url": "/Conways-Life/static/js/1.86e2d1e0.chunk.js"
+  },
+  {
+    "revision": "f32d1a3631ea78450cf3",
+    "url": "/Conways-Life/static/css/main.897e6288.chunk.css"
+  },
+  {
+    "revision": "c92692c889c6720a91e7e3ca5c7c3fac",
+    "url": "/Conways-Life/index.html"
+  }
+];
